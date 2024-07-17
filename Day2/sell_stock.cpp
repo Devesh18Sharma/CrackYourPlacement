dfc883ab7afe7b4f34c185430659a64c5847a6d9
Devesh Sharma <97972618+Devesh18Sharma@users.noreply.github.com>
2024-07-17 15:29:51 +0530

    Create sell_stock.cpp

---
+++ Day2/sell_stock.cpp
@@ -0,0 +1,20 @@
+//prices[i]
+// Kam main kharido jyada main bech do
+//2 pointers 
+//DP
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy=prices[0];
+        int profit=0;
+        for(int i=1;i<prices.size();i++){
+            if(prices[i]<buy){
+                buy=prices[i];
+            }else if(prices[i]-buy>profit){
+                profit=prices[i]-buy;
+            }
+        }
+        return profit;
+    }
+};
