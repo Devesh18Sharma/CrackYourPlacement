a70f3dcc06e66d0e5c5630afc1fc921846007f3b
Devesh Sharma <97972618+Devesh18Sharma@users.noreply.github.com>
2024-07-19 23:23:36 +0530

    Create min_moves_equal_arr.cpp

---
+++ Day4/min_moves_equal_arr.cpp
@@ -0,0 +1,16 @@
+/*
+sort
+find int mid and 
+ans+=abs(mid-val)
+*/
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int mid=nums[nums.size()/2];
+        int ans=0;
+        for(auto val:nums)
+        ans+=abs(mid-val);
+        return ans;
+    }
+};
